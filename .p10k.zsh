b611cd8a8e43457a2f93493fe95d7ef53cfa9ea2
Steven C. Howell <showell@umd.edu>
2022-03-02 13:55:53 -0500

    solarized colors

--- .p10k.zsh
+++ .p10k.zsh
@@ -212,7 +212,7 @@
 
   ##################################[ dir: current directory ]##################################
   # Default current directory color.
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND=033
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND=032  # should be 004, but changed to match anchor forground  
   # If directory is too long, shorten some of its segments to the shortest possible unique
   # prefix. The shortened directory can be tab-completed to the original.
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
@@ -222,7 +222,7 @@
   typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=006
   # Color of the anchor directory segments. Anchor segments are never shortened. The first
   # segment is always an anchor.
-  typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=033  # why different color codes???
+  typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=032  # should be 004... why use different color codes???
   # Display anchor directory segments in bold.
   typeset -g POWERLEVEL9K_DIR_ANCHOR_BOLD=true
   # Don't shorten directories that contain any of these files. They are anchors.
