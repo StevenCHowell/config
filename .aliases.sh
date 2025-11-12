18a8baa7b9aa41afa8c8dd62d3558f6bf124b6a5
Steven C. Howell <showell@arlis.umd.edu>
2025-11-12 17:54:21 +0000

    WIP: fix gpu-box loading

--- .aliases.sh
+++ .aliases.sh
@@ -1,5 +1,5 @@
 # added for dot-file repo: config
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 # nvidia
 alias htop-cuda='watch -n 1 nvidia-smi'
@@ -17,8 +17,8 @@ alias pytest='pytest --disable-warnings'
 alias update="sudo pkcon refresh ; sudo pkcon update"
 alias update_clean="sudo pkcon refresh ; sudo pkcon -y update ; sudo apt -y autoremove --purge ; sudo apt -y autoclean"
 
-## try one of these if pkcon doesn't work well on Ubuntu
-## option 1, generic
+# try one of these if pkcon doesn't work well on Ubuntu
+# option 1, generic
 # if [ -e /etc/lsb-release ]; then
 #     . /etc/lsb-release
 #     if [ ${DISTRIB_ID} = Ubuntu ]; then
@@ -29,7 +29,7 @@ alias update_clean="sudo pkcon refresh ; sudo pkcon -y update ; sudo apt -y auto
 #         alias update_clean="sudo pkcon refresh ; sudo pkcon -y update ; sudo apt -y autoremove --purge ; sudo apt -y autoclean"
 #     fi
 # fi
-## option 2, specific
+#  option 2, specific
 # if [[ ( `hostname -s` = red5 ) ]]; then
 #     alias update="sudo pkcon refresh ; sudo pkcon update"
 #     alias update_clean="sudo pkcon refresh ; sudo pkcon -y update ; sudo apt -y autoremove --purge ; sudo apt -y autoclean"
@@ -40,7 +40,7 @@ alias update_clean="sudo pkcon refresh ; sudo pkcon -y update ; sudo apt -y auto
 
 # Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
-  export EDITOR='vim'
+ export EDITOR='emacs'
 else
   export EDITOR='code'
 fi
@@ -59,7 +59,7 @@ eogn () { for i; do eog -n "$i" & done; }
 
 alias l='ls -1rt --color=tty'
 alias l.="ls -lhtrdF .* --color=tty"
-alias sl="ls --color=tty"
+alias sl="ls -FX --color=tty"
 alias ls="ls -FX --color=tty"
 alias ll="ls -ltrhF --color=tty"
 alias la="ls -a -trhF --color=tty"
@@ -69,15 +69,12 @@ alias lsh="ls -lt --color=tty | head"
 alias lst="ls -lrt --color=tty | tail"
 alias lsa="ls -lrta --color=tty | tail"
 alias lsd='ls -lt | grep drwx | head'
-
 alias dir='dir --color=tty'
-
 alias rm="rm -ir --preserve-root"
-alias trash="for f in $@; do mv $f ~/trashcan; done"
+alias mv="mv -iv"
+alias cp="cp -iva"
 
-# From Joseph
-# alias cdsm='cd ~/data/myPrograms/sasmol/'
-# alias cds2='cd ~/data/codesassie_svn/sassie_2.0/'
+alias trash="for f in $@; do mv $f ~/trashcan; done"
 
 alias df="df -h"
 alias du="du -sh * | sort -h"
@@ -97,7 +94,6 @@ alias svn_id='svn propset svn:keywords "Id"'
 alias dir="lt"
 # alias lock="gnome-screensaver-command -l"
 
-alias xemacs="xemacs21"
 alias topme="htop -u schowell"
 
 # clamav
@@ -116,11 +112,6 @@ alias now='date +"%H:%M:%S"'
 alias nowtime=now
 alias nowdate='date +"%Y%m%d"'
 
-alias vi=vim
-alias svi='sudo vi'
-alias vis='vim "+set si"'
-alias edit='vim'
-
 # Stop after sending count ECHO_REQUEST packets #
 alias ping='ping -c 5'
 # Do not wait interval 1 second, go fast #
@@ -130,10 +121,10 @@ alias ping='time ping'
 # show open ports
 alias ports='netstat -tulanp'
 
-# Parenting changing perms on /
-alias chown='chown --preserve-root'
-alias chmod='chmod --preserve-root'
-alias chgrp='chgrp --preserve-root'
+# # Parenting changing perms on /
+# alias chown='chown --preserve-root'
+# alias chmod='chmod --preserve-root'
+# alias chgrp='chgrp --preserve-root'
 
 # NVIDIA top
 alias nvidia-top='watch -n0.5 nvidia-smi'
@@ -177,23 +168,22 @@ alias xdvi='xdvi -bg white -s 10'
 CVS_RSH="ssh"; export CVS_RSH
 export CVSEDITOR="emacs -nw --no-init-file"
 
-# added for GSL and boost
-[ -d /usr/local/lib ] && export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-
-# added for VMD
-[ -d /home/schowell/data/myPrograms/vm ] && export LD_LIBRARY_PATH=/home/schowell/data/myPrograms/vm:$LD_LIBRARY_PATH
-
-# added for OpenACC Toolkit
-[ -d /opt/pgi/linux86-64/16.7/bin ] && export PATH=/opt/pgi/linux86-64/16.7/bin:$PATH
-
-# add to path
-[ -d ~/.local/bin/ ] && export PATH=~/.local/bin/:$PATH
-[ -d ~/bin/ ] && export PATH=~/bin/:$PATH
-[ -d ~/data/myPrograms/bin/ ] && export PATH=~/data/myPrograms/bin/:$PATH
-[ -d ~/AppData/Local/Packages/PythonSoftwareFoundation.Python*/LocalCache/localPython313/Scripts/ ] && export PATH=~/AppData/Local/Packages/PythonSoftwareFoundation.Python*/LocalCache/localPython313/Scripts/:$PATH
-[ -d "/usr/local/cuda/bin" ] && export PATH="/usr/local/cuda/bin:$PATH"
-[ -d /snap/bin/ ] && export PATH=/snap/bin/:$PATH
-
+# # added for GSL and boost
+# [ -d /usr/local/lib ] && export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+#
+# # added for VMD
+# [ -d /home/schowell/data/myPrograms/vm ] && export LD_LIBRARY_PATH=/home/schowell/data/myPrograms/vm:$LD_LIBRARY_PATH
+#
+# # added for OpenACC Toolkit
+# [ -d /opt/pgi/linux86-64/16.7/bin ] && export PATH=/opt/pgi/linux86-64/16.7/bin:$PATH
+#
+# # add to path
+# [ -d ~/.local/bin/ ] && export PATH=~/.local/bin/:$PATH
+# [ -d ~/bin/ ] && export PATH=~/bin/:$PATH
+# [ -d ~/data/myPrograms/bin/ ] && export PATH=~/data/myPrograms/bin/:$PATH
+# [ -d "/usr/local/cuda/bin" ] && export PATH="/usr/local/cuda/bin:$PATH"
+# [ -d /snap/bin/ ] && export PATH=/snap/bin/:$PATH
+#
 # added for CUDA
 [ -d /usr/local/cuda/lib64 ] && export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 [ -d /usr/local/cuda/bin ] && export PATH=/usr/local/cuda/bin:$PATH
@@ -202,73 +192,74 @@ export CVSEDITOR="emacs -nw --no-init-file"
 # added for theano
 [ -d /usr/local/cuda ] && export CUDA_ROOT=/usr/local/cuda
 
-[ -f ~/.profile ] && . ~/.profile
+[ -d "$HOME/bin" ] && export PATH="$HOME/bin:$PATH"
+[ -d "$HOME/.local/bin" ] && export PATH="$HOME/.local/bin:$PATH"
 
-# de-duplicate pushd
-# https://unix.stackexchange.com/a/288532/83205
-dedup(){
-    declare -a new=() copy=("${DIRSTACK[@]:1}")
-    declare -A seen
-    local v i
-    seen[$PWD]=1
-    for v in "${copy[@]}"
-    do if [ -z "${seen[$v]}" ]
-       then new+=("$v")
-            seen[$v]=1
-       fi
-    done
-    dirs -c
-    for ((i=${#new[@]}-1; i>=0; i--))
-    do      builtin pushd -n "${new[i]}" >/dev/null
-    done
-}
-
-# automatically activate and deactivate Python virtual environments
-# https://stackoverflow.com/a/50830617/3585557
-function cd() {
-  builtin cd "$@"
-
-  if [[ -z "$VIRTUAL_ENV" ]] ; then
-    ## If env folder is found then activate the vitualenv
-      if [[ -d ./venv ]] ; then
-        source ./venv/bin/activate
-      fi
-  else
-    ## check the current folder belong to earlier VIRTUAL_ENV folder
-    # if yes then do nothing
-    # else deactivate
-      parentdir="$(dirname "$VIRTUAL_ENV")"
-      if [[ "$PWD"/ != "$parentdir"/* ]] ; then
-        deactivate
-      fi
-  fi
-}
-
-# ipython
-alias tipython="ipython --pylab"
-alias gipython="ipython notebook --pylab=inline"
-
-# pushd(){
-#     builtin pushd "$@"
-#     dedup
+# # de-duplicate pushd
+# # https://unix.stackexchange.com/a/288532/83205
+# dedup(){
+#     declare -a new=() copy=("${DIRSTACK[@]:1}")
+#     declare -A seen
+#     local v i
+#     seen[$PWD]=1
+#     for v in "${copy[@]}"
+#     do if [ -z "${seen[$v]}" ]
+#        then new+=("$v")
+#             seen[$v]=1
+#        fi
+#     done
+#     dirs -c
+#     for ((i=${#new[@]}-1; i>=0; i--))
+#     do      builtin pushd -n "${new[i]}" >/dev/null
+#     done
 # }
-
-# alias crysol="crysol -lm 50 -fb 18" #xray default
-# alias crysol="crysol -lm 15 -fb 18" #my default (lm is important for MASSHA but takes much longer)
-# alias crysol="crysol -lm 10 -fb 18 -sm 0.2" #my default (lm = Q_max * D_max / pi, and it is not accurate beyond 0.2)
-
-# # added for Launcher (https://github.com/tacc/launcher)
-# #export LAUNCHER_JOB_FILE=<file_to_run>
-# export LAUNCHER_WORKDIR=$HOME/data/myPrograms/launcher/
-
-
-# # Compile & Debug C
-# # alias val="valgrind --leak-check=full -v --show-reachable=yes"
-# alias val="valgrind --tool=memcheck --leak-check=full -v --show-reachable=yes --track-origins=yes"
-# alias valv="valgrind --verbose --tool=memcheck --leak-check=full -v --show-reachable=yes --track-origins=yes"
-# alias gcc='gcc -ansi -std=c99 -pedantic -Wall -g'
-
-# # for OSPRay imcompatibility bug in VMD
-# if [[ ( `hostname -s` = thor ) ]]; then
-#     export VMDNOOSPRAY=1
-# fi
+#
+# # automatically activate and deactivate Python virtual environments
+# # https://stackoverflow.com/a/50830617/3585557
+# function cd() {
+#   builtin cd "$@"
+#
+#   if [[ -z "$VIRTUAL_ENV" ]] ; then
+#     ## If env folder is found then activate the vitualenv
+#       if [[ -d ./venv ]] ; then
+#         source ./venv/bin/activate
+#       fi
+#   else
+#     ## check the current folder belong to earlier VIRTUAL_ENV folder
+#     # if yes then do nothing
+#     # else deactivate
+#       parentdir="$(dirname "$VIRTUAL_ENV")"
+#       if [[ "$PWD"/ != "$parentdir"/* ]] ; then
+#         deactivate
+#       fi
+#   fi
+# }
+#
+# # ipython
+# alias tipython="ipython --pylab"
+# alias gipython="ipython notebook --pylab=inline"
+#
+# # pushd(){
+# #     builtin pushd "$@"
+# #     dedup
+# # }
+#
+# # alias crysol="crysol -lm 50 -fb 18" #xray default
+# # alias crysol="crysol -lm 15 -fb 18" #my default (lm is important for MASSHA but takes much longer)
+# # alias crysol="crysol -lm 10 -fb 18 -sm 0.2" #my default (lm = Q_max * D_max / pi, and it is not accurate beyond 0.2)
+#
+# # # added for Launcher (https://github.com/tacc/launcher)
+# # #export LAUNCHER_JOB_FILE=<file_to_run>
+# # export LAUNCHER_WORKDIR=$HOME/data/myPrograms/launcher/
+#
+#
+# # # Compile & Debug C
+# # # alias val="valgrind --leak-check=full -v --show-reachable=yes"
+# # alias val="valgrind --tool=memcheck --leak-check=full -v --show-reachable=yes --track-origins=yes"
+# # alias valv="valgrind --verbose --tool=memcheck --leak-check=full -v --show-reachable=yes --track-origins=yes"
+# # alias gcc='gcc -ansi -std=c99 -pedantic -Wall -g'
+#
+# # # for OSPRay imcompatibility bug in VMD
+# # if [[ ( `hostname -s` = thor ) ]]; then
+# #     export VMDNOOSPRAY=1
+# # fi
